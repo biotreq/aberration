e66c3bf6a8764d60959ea165ef2b84732438495d
biotreq <biotreq3d@gmail.com>
2024-03-22 21:00:00 +0100

    Use easily scaled linear blur

--- Player/HUD/Blur.gd
+++ Player/HUD/Blur.gd
@@ -11,7 +11,7 @@ func play_damage_feedback():
 	if damage_tween:
 		damage_tween.kill()
 	set_blur_inner(0.0)
-	set_blur_magnitude(50.0)
+	set_blur_magnitude(20.0)
 	damage_tween = create_tween()
 	damage_tween.set_parallel()
 	damage_tween.tween_method(color_red_border, 1.5, 0.0, 1.0)
@@ -19,11 +19,8 @@ func play_damage_feedback():
 
 
 func play_respawn_blur():
-	set_blur_inner(-5.0)
 	damage_tween = create_tween()
-	damage_tween.tween_method(set_blur_magnitude, 50.0, 5.0, 5.0)
-	damage_tween.tween_method(set_blur_inner, -5.0, 0.3, 8.0)
-	damage_tween.tween_method(set_blur_magnitude, 5.0, 50.0, 2.0)
+	damage_tween.tween_method(set_blur_inner, -1.0, 0.3, 12.0)
 
 
 func color_red_border(amount: float):
